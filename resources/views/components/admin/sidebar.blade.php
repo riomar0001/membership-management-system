8f3bc5e65ab639dc8e9282f1805d6072ec5223fb
Jade Palma <jedzxcx1@gmail.com>
2025-03-04 22:37:53 +0800

    ..

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -104,7 +104,7 @@
                                                 <a class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:bg-neutral-800 dark:text-neutral-200"
                                                     href="#">
                                                     Link 1
-                                                </a>
+                                                git merge your-branch-name                                                </a>
                                             </li>
                                             <li>
                                                 <a class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:bg-neutral-800 dark:text-neutral-200"
