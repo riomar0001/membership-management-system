e9253e8d4bf7381a480194828715e6f594c13d0b
Jade Palma <jedzxcx1@gmail.com>
2025-03-14 15:10:47 +0800

    nav bar inconsitency fixed

--- resources/views/components/landing/header.blade.php
+++ resources/views/components/landing/header.blade.php
@@ -7,12 +7,12 @@
 
         <nav id="navmenu" class="navmenu">
             <ul>
-                <li><a href="#hero" class="active">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#purpose">Purpose</a></li>
-                <li><a href="#goals">Goals</a></li>
-                <li><a href="#team">Team</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li><a href="/#hero" class="active">Home</a></li>
+                <li><a href="/#about">About</a></li>
+                <li><a href="/#purpose">Purpose</a></li>
+                <li><a href="/#goals">Goals</a></li>
+                <li><a href="/#team">Team</a></li>
+                <li><a href="/#contact">Contact</a></li>
             </ul>
             <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
         </nav>
